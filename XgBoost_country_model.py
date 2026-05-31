8810593f6c01773c871fe494d3e9968f5918ed0a
Dr. Laxmi Pandey <68345566+LAXMI87848@users.noreply.github.com>
2026-06-01 05:06:01 +0530

    Update XgBoost_country_model.py

--- XgBoost_country_model.py
+++ XgBoost_country_model.py
@@ -43,8 +43,8 @@ single_run_dir = os.path.join(output_dir, "single_run_results")
 os.makedirs(single_run_dir, exist_ok=True)
 
 sheet_name = "Sheet1"
-# Load your data
-excelFilePath = r'G:\IIT_AI work\ISRAEL\GWS_Model info\Data and feature score\Revised data and feature_fitting scores\dataset_28.8.xlsx'
+# Load your data and give your own path in #excelFilePath, where the data shared is been saved
+excelFilePath = r'G:\IIT_AI work\ISRAEL\GWS_Model info\Data and feature score\Revised data and feature_fitting scores\Dataset_to_share.xlsx'
 df = pd.read_excel(excelFilePath, sheet_name)
 basin_data = df['Hydrological Basin']
 
